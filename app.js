f49e5daa6ce0855d2a97b4e9ff6a69d904ddd3b2
LaithLou <laithlouay@gmail.com>
2022-05-31 19:24:57 -0400

    we comunicated with api and loged result

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+"use strict";
 console.log("Let's get this party started!");
+
+$("<form id='main'>").appendTo("body");
+$('<input id="search" placeholder="search">').appendTo("form");
+$('<input id="button" value="search" type="button">').appendTo("form");
+let $container = $('<div id="container">').appendTo("body");
+//axios.get("giphy.com", { parma: { a: 1, b: 2 } });
+
+$('#main').on('submit', function(){
+
+  async function api() {
+    let response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+      params: { q: "CATS", api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
+    });
+    return response;
+  }
+
+
+  let $search = $("#search").val();
+
+  })
+
+
+
+})
+
+async function api() {
+  let response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+    params: { q: "CATS", api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
+  });
+  return response;
+}
