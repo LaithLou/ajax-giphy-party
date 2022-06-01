1b82582e0be56070329ec12503acbd1122650daf
LaithLou <laithlouay@gmail.com>
2022-05-31 20:47:44 -0400

    appeded imgs to page

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 "use strict";
 console.log("Let's get this party started!");
-
+//no need for jQuery we can add it over html
 $("<form id='main'>").appendTo("body");
 $('<input id="search" placeholder="search">').appendTo("form");
-$('<input id="button" value="search" type="button">').appendTo("form");
+$(
+  '<button id="button" value="search" type="submit"> search </button>'
+).appendTo("form");
 let $container = $('<div id="container">').appendTo("body");
 //axios.get("giphy.com", { parma: { a: 1, b: 2 } });
-let $search = $("#search").val();
 
-// $('#main').on('submit', api)
-//   async function api() {
-//     let response = await axios.get("https://api.giphy.com/v1/gifs/search", {
-//       params: { q: "CATS", api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
-//     });
-//     return response;
-//   }
+// make this do less work - less lines
+$("form").on("submit", async function (evt) {
+  evt.preventDefault();
 
+  let $search = $("#search").val();
 
-//     let $search = $("#search").val();
-//     await api($search)
-
+  let response = await api($search);
+  console.log(response);
+  //would be nice to move into the api function.
+  let resultImg = response.data.data[0].images.original.url;
+  //restructer
+  let $img = $(`<img>`).attr("src", resultImg);
+  $($img).appendTo("#container");
+});
 
 async function api(search) {
-    $('document').ready(mainfunction);
-    let response = await axios.get("https://api.giphy.com/v1/gifs/search", {
-        params: { q: search, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
-    });
-    return response;
+  // $("document").ready(mainfunction);
+  let response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+    params: { q: search, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
+  });
+  return response;
 }
 
-let apiObj = await api($search);
-console.log(apiObj.data.data[0].url);
-
 // update "CAT" to our submit value
-// preventdefault()
+// preventDefault()
 
 // 1. get gif (variable.data.data[0].url)
 // 2. append to a page (our container)
-// 3. 
+// 3.
